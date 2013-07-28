d59a69a9066ece435a7a839a9cfbd925233035b7
Stan Bobovych <stan.bobovych@gmail.com>
2013-07-27 23:08:41 -0400

    Added C# based injection library and C# injector

---
+++ VS12/WinHookInject/DllInjector/DllInjector.cs
@@ -0,0 +1,29 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+using DllInjection;
+
+namespace InjectorApp
+{
+    class Program
+    {
+        static void Main(string[] args)
+        {
+            if (args.Length < 2)
+            {
+                System.Console.WriteLine("Usage: {0} program.exe library.dll", System.AppDomain.CurrentDomain.FriendlyName);
+                return;
+            }
+            
+            string sProcName = args[0];
+            string sDllPath = args[1];
+            System.Console.WriteLine("{0} {1}", sProcName, sDllPath);
+            DllInjector inj = DllInjector.GetInstance;
+            DllInjectionResult result = inj.Inject(sProcName, sDllPath);
+            System.Console.WriteLine(result);
+        }
+    }
+}
